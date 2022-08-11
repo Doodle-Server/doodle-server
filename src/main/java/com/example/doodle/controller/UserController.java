0ab9c51b3629d5feecb83afa5dd0eee7dcab182b
ParkIsComing <maroony55@gmail.com>
2022-08-11 14:45:24 +0900

    회원가입 수정

--- src/main/java/com/example/doodle/controller/UserController.java
+++ src/main/java/com/example/doodle/controller/UserController.java
@@ -2,38 +2,35 @@ package com.example.doodle.controller;
 
 import com.example.doodle.domain.User;
 import com.example.doodle.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
     @Autowired
     UserService userService;
 
 
-    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    @RequestMapping(value = "/users/signup", method = RequestMethod.GET)
     public String getsignup(Locale locale, Model model) {
-        Date date = new Date();
-        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-        String formattedDate = dateFormat.format(date);
-        model.addAttribute("serverTime", formattedDate );
+
         return "signup";
     }
 
-    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    @RequestMapping(value = "/users/signup", method = RequestMethod.POST)
     public String postsignup(User user){
         userService.createUser(user);
-        return "redirect:/";
+        return "index";
     }
 
-    @GetMapping("/getUserById.do")
+    @GetMapping("getUserById.do")
     @ResponseBody
     public String getUserById(@RequestParam String userid) {
 
--- src/main/java/com/example/doodle/mapper/UserMapper.java
+++ src/main/java/com/example/doodle/mapper/UserMapper.java
@@ -3,8 +3,12 @@ package com.example.doodle.mapper;
 import com.example.doodle.domain.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
     void createUser(User user);
     User getUserById(String userid);
+    List<Map<String, Object>> findAll();
 }

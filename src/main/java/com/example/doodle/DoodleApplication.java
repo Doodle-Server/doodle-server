bf767f437df3cbe021b48e68997aae4420a7b7b9
ParkIsComing <maroony55@gmail.com>
2022-09-01 22:06:32 +0900

    websocket

--- src/main/java/com/example/doodle/DoodleApplication.java
+++ src/main/java/com/example/doodle/DoodleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.doodle.controller", "com.example.doodle.domain", "com.example.doodle.mapper","com.example.doodle.service","com.example.doodle.dto"})
+@ComponentScan(basePackages = {"com.example.doodle.controller", "com.example.doodle.mapper","com.example.doodle.service","com.example.doodle.dto","com.example.doodle.websocket"})
 @EnableJpaRepositories
 @MapperScan(basePackages = {"com.example.doodle.dao","com.example.doodle.mapper"})
 public class DoodleApplication {
